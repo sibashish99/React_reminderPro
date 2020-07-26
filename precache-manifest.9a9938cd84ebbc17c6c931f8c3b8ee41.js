30560938ea6d496abbad3b931a7f78da55e07e0e
sibashish biswas <sibashishbiswas349@gmail.com>
2020-07-26 15:40:51 +0530

    Updates

---
+++ precache-manifest.9a9938cd84ebbc17c6c931f8c3b8ee41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bc68957174c62949d67a00c3f3a5c01",
+    "url": "/React_reminderPro/index.html"
+  },
+  {
+    "revision": "85b48d7b14ea20e43e6e",
+    "url": "/React_reminderPro/static/css/main.0c7bb98d.chunk.css"
+  },
+  {
+    "revision": "f36f1fdbee1ffd25840d",
+    "url": "/React_reminderPro/static/js/2.61b54893.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React_reminderPro/static/js/2.61b54893.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85b48d7b14ea20e43e6e",
+    "url": "/React_reminderPro/static/js/main.29a68f57.chunk.js"
+  },
+  {
+    "revision": "07220467d74a3f534d68",
+    "url": "/React_reminderPro/static/js/runtime-main.cf75a3b8.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/React_reminderPro/precache-manifest.9a9938cd84ebbc17c6c931f8c3b8ee41.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React_reminderPro/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
